d14f17bf4f76c64071b3b57bb146b22686a78877
Maika Fujii <fujii108@mail.chapman.edu>
2017-11-28 00:14:49 -0500

    Fixed Headers

--- Player.java
+++ Player.java
@@ -1,3 +1,21 @@
+/**
+ * Collaborators:
+ * Maika Fujii
+ *  ID: 1935412
+ *  fujii108@mail.chapman.edu /**
+ * Thomas Madden
+ *  ID: 2261821
+ *  madde120@mail.chapman.edu /**
+ * Dillon Tidgewell
+ *  ID:
+ *  tidge@mail.chapman.edu /**
+ *
+ * Course: CPSC 353-01
+ * Assignment: Final Project - Tic-Tac-Toe
+ *
+ *
+ * Player.java
+ */
 import java.net.*;
 
 public class Player {
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,21 @@
-
+/**
+ * Collaborators:
+ * Maika Fujii
+ *  ID: 1935412
+ *  fujii108@mail.chapman.edu /**
+ * Thomas Madden
+ *  ID: 2261821
+ *  madde120@mail.chapman.edu /**
+ * Dillon Tidgewell
+ *  ID:
+ *  tidge@mail.chapman.edu /**
+ *
+ * Course: CPSC 353-01
+ * Assignment: Final Project - Tic-Tac-Toe
+ *
+ *
+ * TicTacToe.java
+ */
 //import javax.*;
 import javax.swing.JFrame;
 import javax.swing.*;
